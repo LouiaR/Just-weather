9eed2d724257b520be6b528cc10f527f1e7a4710
louiaR <louitiani@gmail.com>
2018-08-29 13:35:05 +0100

    Set up server

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const path = require('path');
+const apiRoute = require('./routes/api');
+
+const app = express();
+
+require('dotenv').config({ path: '.env' });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', apiRoute);
+
+app.use(function(req,res){  
+	res.status(404).render('notFoundPage', { error: 'page not found'}); 
+});
+
+app.set('port', process.env.PORT || 2008);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Server running at PORT ${server.address().port}`);
+});
